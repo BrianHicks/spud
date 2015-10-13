03c7665d97f83f7b9e3a1f58d7eeed2715d3cd08
Brian Hicks <brian@brianthicks.com>
2015-10-13 08:48:53 -0500

    Trello: move lots of stuff over

--- Main.elm
+++ Main.elm
@@ -1,30 +1,22 @@
 module Spud where
 
-import StartApp
-import Trello
 import Effects
-import Html exposing (..)
+import StartApp
 import Task
+import Trello
+import Signal
+import Html exposing (Html)
 
-port token : Signal Trello.Token
-
-view : Signal.Address Trello.TokenAction -> Trello.Token -> Html.Html
-view address model =
-  case model of
-    Nothing ->
-      div [] [text "token not set"]
-
-    Just token ->
-      div [] [text token]
-
-app : StartApp.App Trello.Token
-app = StartApp.start { init = (Trello.model, Effects.none)
+app : StartApp.App Trello.Model
+app = StartApp.start { init = (Trello.init, Effects.none)
                      , inputs = [ Signal.map Trello.toNewToken token ]
                      , update = Trello.update
-                     , view = view }
+                     , view = Trello.view }
 
 main : Signal Html
 main = app.html
 
 port tasks : Signal (Task.Task Effects.Never ())
 port tasks = app.tasks
+
+port token : Signal (Maybe String)
--- Trello.elm
+++ Trello.elm
@@ -1,20 +1,45 @@
-module Trello (Token, TokenAction, toNewToken, model, update) where
+module Trello (Model, init, toNewToken, update, view) where
 
-import Effects exposing (none)
+import Effects
+import Html exposing (Html, div, text)
+import Html.Attributes exposing (class)
+import Signal
 
--- token actions
-type TokenAction = NewToken Token
-
-toNewToken : Token -> TokenAction
-toNewToken tkn = NewToken tkn
+-- MODEL
 
 type alias Token = Maybe String
 
--- token models
-model : Token
-model = Nothing
+type alias Model = { token : Token }
 
-update : TokenAction -> Token -> (Token, Effects.Effects TokenAction)
+init : Model
+init = { token = Nothing }
+
+authenticated : Model -> Bool
+authenticated model =
+  case model.token of
+    Nothing -> False
+    Just _  -> True
+
+-- UPDATE
+
+type Action
+  = NewToken Token
+
+toNewToken : Token -> Action
+toNewToken token = NewToken token
+
+update : Action -> Model -> (Model, Effects.Effects Action)
 update action model =
   case action of
-    NewToken token -> (token, none)
+    NewToken token -> ({ model | token <- token }, Effects.none)
+
+-- VIEW
+
+authView : Signal.Address Action -> Model -> Html
+authView address model =
+  div [class (if authenticated model then "authenticated" else "unauthenticated")]
+      [text (if authenticated model then "Authenticated" else "Not Yet Authenticated")]
+
+view : Signal.Address Action -> Model -> Html
+view address model =
+  authView address model
